78b6d42773301aa298b92b20daa655a8860133a9
lelanthran <lelanthran@gmail.com>
2021-02-06 14:20:27 +0200

    WIP-interfaces: committing so that I can rename everything.

--- src/netcode_ifs.h
+++ src/netcode_ifs.h
@@ -1,11 +1,22 @@
 #ifndef H_NETCODE_IFS
 #define H_NETCODE_IFS
 
+typedef struct netcode_ifs_t netcode_ifs_t;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+   netcode_ifs_t **netcode_ifs_list_new (void);
+   void netcode_ifs_list_del (netcode_ifs_t **list);
+
+   bool netcode_ifs_extract (const netcode_ifs_t *ifs,
+                             uint64_t  *dst_if_flags,
+                             char **dst_if_name,
+                             char **dst_if_addr,
+                             char **dst_if_netmask,
+                             char **dst_if_broadcast,
+                             char **dst_if_p2paddr);
 
 #ifdef __cplusplus
 };
--- src/netcode_ifs_test.c
+++ src/netcode_ifs_test.c
@@ -7,7 +7,31 @@
 
 static int ifs_test (void)
 {
-   return EXIT_FAILURE;
+   int ret = EXIT_FAILURE;
+
+   netcode_ifs_t **list = netcode_ifs_list_new ();
+
+   if (!list) {
+      NETCODE_UTIL_LOG ("Failed to get list of interfaces\n");
+      goto errorexit;
+   }
+
+   for (size_t i=0; list[i]; i++) {
+   bool netcode_ifs_extract (const netcode_ifs_t *ifs,
+                             uint64_t  *dst_if_flags,
+                             char **dst_if_name,
+                             char **dst_if_addr,
+                             char **dst_if_netmask,
+                             char **dst_if_broadcast,
+                             char **dst_if_p2paddr);
+   }
+
+   ret = EXIT_SUCCESS;
+errorexit:
+
+   netcode_ifs_list_del (flist);
+
+   return ret;
 }
 
 int main (void)
