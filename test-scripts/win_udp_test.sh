b84d6936d55d383ba4a215cbcea1ab0483eb2e9f
lelanthran <lelanthran@gmail.com>
2021-02-07 17:30:54 +0200

    Test scripts for running under Wine works. Tests not passing for UDP.

--- test-scripts/win_udp_test.sh
+++ test-scripts/win_udp_test.sh
@@ -4,5 +4,13 @@ if [ `uname -a | grep -ci linux` -gt 0 ]; then
    export PREFIX=wine
 fi
 
-$PREFIX ../debug/bin/x86_64-w64-mingw32/netcode_server_test.exe udp_test & sleep 1 ;\
-$PREFIX ../debug/bin/x86_64-w64-mingw32/netcode_client_test.exe udp_test
+rm win_server_result.txt
+rm win_client_result.txt
+
+$PREFIX ../debug/bin/x86_64-w64-mingw32/netcode_server_test.exe udp_test &> win_server_result.txt &
+sleep 1
+$PREFIX ../debug/bin/x86_64-w64-mingw32/netcode_client_test.exe udp_test &> win_client_result.txt
+
+cat win_server_result.txt
+cat win_client_result.txt
+
